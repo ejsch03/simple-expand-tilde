fe236cfab389f62e87c580ede640fcf146a65459
splurf <evandotschwartzentruber@gmail.com>
2026-02-19 11:37:10 -0500

    refactored, rm deprecation label

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,11 @@ use std::path::*;
 pub fn expand_tilde(path: impl AsRef<Path>) -> Option<PathBuf> {
     let p = path.as_ref();
 
-    let expanded = if p.starts_with("~") {
-        #[allow(deprecated)]
+    if p.starts_with("~") {
         let mut base = std::env::home_dir()?;
-
-        if !p.ends_with("~") {
-            base.extend(p.components().skip(1));
-        }
-        base
+        base.extend(p.components().skip(1));
+        Some(base)
     } else {
-        p.to_path_buf()
-    };
-    Some(expanded)
+        Some(p.to_path_buf())
+    }
 }
